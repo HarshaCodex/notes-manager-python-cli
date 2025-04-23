93ce7bec37ed11f4fbdb16fdc1a5b7d26da381a7
HarshaCodex <harshacodex@gmail.com>
2025-04-22 20:49:31 -0500

    Wrap menu in loop

--- notes-manager/main.py
+++ notes-manager/main.py
@@ -8,5 +8,29 @@ menu = '''1. Add a new note
 6. Exit
 '''
 
-print(header)
-print(menu)
+while True:
+
+    print(header)
+    print(menu)
+
+    try:
+        choice = int(input("Choose your choice: "))
+    except ValueError:
+        print("Invalid input! Please enter a number between 1 and 6.\n")
+        continue
+
+    if choice == 1:
+        print('📌 You chose to add a new note.\n')
+    elif choice == 2:
+        print('📋 You chose to view all notes.\n')
+    elif choice == 3:
+        print('🔍 You chose to search notes by keyword.\n')
+    elif choice == 4:
+        print('✏️ You chose to update a note.\n')
+    elif choice == 5:
+        print('🗑️ You chose to delete a note.\n')
+    elif choice == 6:
+        print('👋 Exiting... Goodbye!')
+        break
+    else:
+        print('Please enter a valid choice between 1 and 6.\n')
